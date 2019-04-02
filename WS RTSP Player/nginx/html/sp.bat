31e321bb9d1dffded43d56727371c0f909a8c255
nhnegroup <40194402+nhnegroup@users.noreply.github.com>
2019-04-02 11:44:38 +0900

    a

--- WS RTSP Player/nginx/html/sp.bat
+++
@@ -1,8 +0,0 @@
-cd C:\Program Files\Streamedian\WS RTSP Proxy Server
-
-websock_rtsp_proxy -remove
-
-websock_rtsp_proxy -install
-
-websock_rtsp_proxy -start
-
--- 재난데이터.sql
+++
@@ -1,8 +0,0 @@
---재난안전 데이터 (dispatch_time을 현재시간보다 3시간 전 안쪽으로 변경)
-update tb_safety_occur_list set dispatch_time = to_char(sysdate, 'YYYYMMDDHH24MISS') , process_state = '출동중' where  register_time > '20170613143353'
-
-SELECT * FROM tb_safety_occur_list where  register_time > '20170613143353' AND SAFETY_CATEGORY_CD = '1';
-
-update tb_safety_occur_list set dispatch_time = to_char(sysdate, 'YYYYMMDDHH24MISS') , process_state = '출동중' where  register_time > '20170613143353'
-
-SELECT A.ROWID, A.* FROM tb_safety_occur_list A where  register_time >= '20170613151457' ORDER BY SAFETY_CATEGORY_CD ASC, REGISTER_TIME DESC;
